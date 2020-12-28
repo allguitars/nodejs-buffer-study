7a3c42bd661cce823d5a95fcbf660029bc0a36a1
Dave Lee <isho.lee@gmail.com>
2020-12-28 15:27:24 +0800

    found that JS's string operation has a limitation, so even we slice t the buffer down, we still have trouble combining the result. The result will be too long.

--- buffer-slicing.js
+++ buffer-slicing.js
@@ -1,12 +1,13 @@
 const fs = require('fs');
 
-const dataBuffer = fs.readFileSync('sample.bin');
-const testBuffer = Buffer.from('This is a sentence that should be long enough to slice', 'utf-8');
+const dataBuffer = fs.readFileSync('./dummy-files/sample.bin');
+const testBuffer = Buffer.from('This is a sentence that should be long enough to slice.');
 
-const SLICE_WINDOW = 1000; // Bytes
+// Three octets can be converted to sextets without adding paddings.
+const SLICE_WINDOW = 99;
 
-// const bufferLength = dataBuffer.length;
-// console.log(bufferLength);
+const bufferLength = dataBuffer.length;
+console.log('Buffer length:', bufferLength, 'Bytes');
 
 // const sliceCount = Math.ceil(bufferLength / SLICE_WINDOW);
 // console.log(sliceCount);
@@ -15,19 +16,34 @@ let start = 0;
 let end = SLICE_WINDOW;
 let base64String = '';
 
+/**
+ * Compare the two Base64 outputs that are converted with pre-slicing and without.
+ * base64String will eventually go too big and fail to concatenate more string.
+ */
 // const base64StringNoSlice = Buffer.from(dataBuffer).toString('base64');
-base64StringNoSlice = testBuffer.slice(0, 20).toString('base64');
-console.log(base64StringNoSlice);
 
-base64String += testBuffer.slice(0, 10).toString('base64');
-base64String += testBuffer.slice(10, 20).toString('base64');
-console.log(base64String);
+while (start < bufferLength) {
+  base64String += dataBuffer.slice(start, end).toString('base64');
 
-// while (end < bufferLength) {
-//   base64String += dataBuffer.slice(start, end).toString('base64');
+  start += SLICE_WINDOW;
+  end += SLICE_WINDOW;
+}
+// console.log(base64StringNoSlice === base64String);
 
-//   start += SLICE_WINDOW;
-//   end += SLICE_WINDOW;
-// }
+/**
+ * https://en.wikipedia.org/wiki/Base64#Examples
+ * This is to test the knowledge of base64 converting.
+ * If the input is a multiple of three, then there is no need to add paddings.
+ */
+// base64StringNoSlice = testBuffer.slice(0, 6).toString('base64');
+// console.log(base64StringNoSlice);
 
-console.log(base64StringNoSlice === base64String);
+// base64String += testBuffer.slice(0, 3).toString('base64');
+// base64String += testBuffer.slice(3, 6).toString('base64');
+// console.log(base64String);
+
+// console.log(base64StringNoSlice === base64String);  // true
+
+
+
+
